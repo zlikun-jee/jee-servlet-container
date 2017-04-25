1f2b4517e68ace62536e4660c0d4724b258518cd
zlikun <zlikun-dev@hotmail.com>
2017-04-25 11:46:48 +0800

    处理ServletContext配置

---
+++ zlikun-servlet-container-jetty-9.2.x-embedded/src/main/java/com/zlikun/sc/ServletContextMain.java
@@ -0,0 +1,40 @@
+package com.zlikun.sc;
+
+import com.zlikun.sc.servlet.HelloServlet;
+import com.zlikun.sc.servlet.NotFoundServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+/**
+ * @auther zlikun <zlikun-dev@hotmail.com>
+ * @date 2017/4/25 11:40
+ */
+public class ServletContextMain {
+
+    public static void main(String[] args) throws Exception {
+
+        Server server = new Server(8080);
+
+        ServletContextHandler context = new ServletContextHandler(
+                ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+//        context.setResourceBase(System.getProperty("java.io.tmpdir"));
+        server.setHandler(context);
+
+        // http://localhost:8080/
+        // http://localhost:8080/hello
+
+        // Add dump servlet
+        context.addServlet(HelloServlet.class, "/hello/*");
+        // Add default servlet
+//        context.addServlet(DefaultServlet.class, "/");
+        // 访问不存在的URI时，实际将由NotFoundServlet来处理响应(404)
+        context.addServlet(NotFoundServlet.class, "/");
+
+        server.start();
+        server.join();
+
+    }
+
+}
---
+++ zlikun-servlet-container-jetty-9.2.x-embedded/src/main/java/com/zlikun/sc/servlet/NotFoundServlet.java
@@ -0,0 +1,21 @@
+package com.zlikun.sc.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @auther zlikun <zlikun-dev@hotmail.com>
+ * @date 2017/4/25 11:43
+ */
+public class NotFoundServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.sendError(HttpServletResponse.SC_NOT_FOUND ,"access page is not found !");
+    }
+}
+
